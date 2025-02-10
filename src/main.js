665b1c714b630895b128f86594f0d550cc2d93e7
sidemarschimmelpfennig <sidemarschimmelpfennig@outlook.com>
2025-02-10 00:55:45 -0300

    install and configure modules

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-/* eslint-disable */
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-import './assets/tailwind.css'
+import "./assets/tailwind.css";
+import VueAxios from "vue-axios";
 
 // Create a Vue application instance
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(store).use(router).use(VueAxios).mount("#app");
