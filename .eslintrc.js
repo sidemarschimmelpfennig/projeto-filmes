1056e343613294280df453af451f4c34518c7f66
sidemarschimmelpfennig <sidemarschimmelpfennig@outlook.com>
2025-02-09 22:30:30 -0300

    inite project

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "eslint:recommended", // Certifique-se de manter as aspas simples aqui
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "linebreak-style": "off",
+  },
+};
---
+++ src/main.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import './assets/tailwind.css'
+
+// Create a Vue application instance
+createApp(App).use(store).use(router).mount("#app");
